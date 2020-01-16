972677ef8e59df958f7347c8f8649843281dbe80
Greg <24781574+gpeppel@users.noreply.github.com>
2020-01-16 15:21:38 -0500

    Add files via upload

---
+++ BitcoinTicker/BitcoinData.swift
@@ -0,0 +1,18 @@
+//
+//  BitcoinData.swift
+//  BitcoinTicker
+//
+//  Created by Gregory Peppel on 7/8/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class BitcoinData {
+    
+    var bitcoinPrice : Double = 0
+    
+//    func updateBitconData(condition: String) -> String {
+//        return
+//    }
+}
